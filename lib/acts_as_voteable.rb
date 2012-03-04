edc911f3430459e926b6745d76ee720681255ccc
John Tjanaka <zonecheung@gmail.com>
2012-03-04 16:15:56 -0500

    move vote.rb to app/models and define the Engine.

--- lib/acts_as_voteable.rb
+++ lib/acts_as_voteable.rb
@@ -1,3 +1,14 @@
 # Include hook code here
 require 'acts_as_voteable/acts_as_voteable'
 ActiveRecord::Base.class_eval { include Juixe::Acts::Voteable }
+
+
+module Juixe
+  module Acts
+    module Voteable
+      class Engine < Rails::Engine
+      end if defined?(Rails) && Rails::VERSION::MAJOR == 3
+    end
+  end
+end
+
